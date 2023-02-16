7612b28ddf83bac6c3673c35893170d6f3885f5f
0xNunana <kudayapaul@gmail.com>
2023-02-16 20:45:23 +0000

    User Profile layout

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,11 @@ const Profile = () => {
     <div >
         <Header/>
         <div className=' border shadow-xl max-w-screen-md mx-auto '>
-            <div className='flex justify-center border'>info</div>
+            <div className='flex justify-center border'>
+                <img/> 
+                <p>Info</p>
+               
+                </div>
             <div className='flex justify-center space-x-8'><div>
                 <button className='bg-green-300 rounded-md'>Update</button>
             </div>
