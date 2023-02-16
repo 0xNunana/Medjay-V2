1398a45d5eb2163d645d8a45efa74ba02c50dae5
0xNunana <kudayapaul@gmail.com>
2023-02-16 20:47:26 +0000

    enhanced UI

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer2'
-import { FaBiohazard, FaSearch } from 'react-icons/fa'
+import {  FaSearch } from 'react-icons/fa'
+import DashImage from '../components/DashImage'
 
 const Dashboard = () => {
   return (
     <div>
-        <Header/>
+        <div className="sticky top-0 z-40">
+             <Header />
+        </div>
+       
+        <DashImage/>
         <div className='flex'>
             <div  className='border bg-black flex-shrink-0 pb-3'>
                 <form className='hidden lg:inline p'>
@@ -30,7 +35,7 @@ const Dashboard = () => {
                 
             
             </div>
-            <div className='border bg-slate-400 flex-grow '>
+            <div className='border bg-gradient-to-b from-slate-100 via-slate-100 to-yellow-50  flex-grow '>
                 <div class=" hidden lg:flex justify-evenly">
               <div><button className='bg-white w-full px-5 rounded-b-lg'>Admin</button></div>
                     
@@ -41,16 +46,19 @@ const Dashboard = () => {
               
               
                 </div>
-              <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 shadow-lg pl-5 pt-5'>
-                <div className='text-center'>buy</div>
-                <div className='text-center'>sell</div>
-                <div className='text-center'>use</div>
-                <div className='text-center'>stats</div>
+              <div className='grid sm:grid-cols-1 sm:pt-[30px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 shadow-lg pl-5 pt-5 pb-5'>
+                <div className='text-center pt-5 pr-4 font-serif ' id="Price">Pricing
+                <p className='bg-white rounded-md '>Our pricing is based on speed instead of size. Hence it has no cap as long as you are within the time frame for which you purchased</p>
+                </div>
+                <div className='text-center pt-5'>Features</div>
+                <div className='text-center pt-5'>use</div>
+                <div className='text-center pt-5'>stats</div>
 
               </div>
                 </div>
         </div>
-        <Footer/>
+        <div className='sticky bottom-0'><Footer/></div>
+        
     </div>
   )
 }
